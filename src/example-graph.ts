99437923e6f17e57ad9d59dc9d0f9f1ce4cfa7c6
Dustin Lacewell <dustin@volleythat.com>
2022-04-14 11:14:13 -0500

    Add example graph code

---
+++ src/example-graph.ts
@@ -0,0 +1,20 @@
+
+
+const plt = require('matplotnode');
+const x = new Array(100).fill(0).map((x, i) => i / Math.PI);
+
+// xkcd-style plot :)
+plt.xkcd();
+
+plt.subplot("211");
+plt.title('trig');
+plt.plot(x, x.map(Math.sin), 'color=r', 'label=sin(x)');
+plt.plot(x, x.map(Math.cos), 'color=g', 'label=cos(x)');
+plt.legend();
+
+plt.subplot("212");
+plt.plot(x, x.map(Math.sin).map((t, i) => t * i), 'color=b', 'label=x * sin(x)', 'marker=o', 'linestyle=None');
+plt.legend();
+plt.ylim(-100, 100);
+
+plt.save("./output/test-plot.png");
